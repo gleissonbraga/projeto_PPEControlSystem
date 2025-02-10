c260a508a3a007d1ff5f68899a3d8cb85096e3df
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2025-02-10 20:32:29 -0300

    first commit branch category-epi

---
+++ src/repository/CategoryEpiRepository.ts
@@ -0,0 +1,6 @@
+import { Database } from "../config/db"
+
+const openDbConnection = Database.openDbConnection
+const endDbConnection = Database.endDbConnection
+const db_query = Database.db_query
+const db_query_params = Database.db_query_params
